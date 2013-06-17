5f94c17a97892f490ad040d13fe5ed3bc39cd663
Noah Isaacson <noah@nisaacson.com>
2013-06-17 16:33:20 -0600

    remove eyespect from cli.js

--- cli.js
+++ cli.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 var numberStringRepresenation = require('./')
-var inspect = require('eyespect').inspector({pretty: false})
 var usage = 'Convert a number to its string represenation.\nUsage: $0 --number 2523.04\nOutputs: "Two thousand five hundred twenty-three and 04/100 dollars'
 var argv = require('optimist').usage(usage).alias('n', 'number').describe('number', 'the decimal representation of a number to convert into a string representation').demand('number').argv
 var input  = argv.number
@@ -13,6 +12,6 @@ catch (err) {
   // stack traces look better with newlines printed instead of escaped \n characters
   var stack = err.stack
   delete err.stack
-  inspect(err, 'error converting input number to string representation')
+  console.dir(err, 'error converting input number to string representation')
   console.log(stack)
 }
