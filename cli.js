e1c6be010aa2ca3c1f59dd533ddc48928d9ea9af
Noah Isaacson <noah@nisaacson.com>
2013-06-18 11:34:04 -0600

    added missing comments

--- cli.js
+++ cli.js
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
+
+/**
+ * make number-string-representation available as a command line binary tool
+ * when the module is install globally using the "-g" or "--global" flag
+ */
 var numberStringRepresenation = require('./')
 var usage = 'Convert a number to its string represenation.\nUsage: $0 --number 2523.04\nOutputs: "Two thousand five hundred twenty-three and 04/100 dollars'
 var argv = require('optimist').usage(usage).describe('number', 'the decimal representation of a number to convert into a string representation').demand('number').argv
@@ -10,7 +15,7 @@ try {
 
 catch (err) {
   // stack traces look better with newlines printed instead of escaped \n characters
-  var stack = err.stack
+  var stack = err.stack;
   delete err.stack
   console.error(err)
   console.error(stack)
--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-/**
- * number-string-represenation
- * @param  {String} a string version of a decimal number
- *   for which the function should get representation for
- * @return {String}       The string representation for the input number
- *
- * @example if input === 2523.04, the function should return
- *   "Two thousand five hundred twenty-three and 04/100 dollars"
- */
-
 var getStringRepresentation = require('./lib/getStringRepresentation')
-
-
 module.exports = numberStringRepresentation;
 
+/**
+ * number-string-represenation
+ * @param  {Number} a decimal number for which the module should return a
+ * string representation
+ * @return {String} The string representation for the input number
+ *
+ * @example if the input is the number 2523.04, the module should return
+ *   "Two thousand five hundred twenty-three and 04/100 dollars"
+ */
 function numberStringRepresentation(input) {
   var error = validateInput(input)
   if (error) {
@@ -27,7 +24,6 @@ function numberStringRepresentation(input) {
  * @return {Error} if the type of the input is a valid number, return null,
  *   otherwise return an error object
  */
-
 function validateInput(input) {
   var error, typeOfInput
     typeOfInput = typeof input
@@ -44,10 +40,13 @@ function validateInput(input) {
 
 /**
  * validateSize
- * @param  {Number} input the input decimal number to convert to a string representation
- * @return {Error} null if the input is within the supported range, an error object otherwise
+ *
+ * Make sure that the input number is within the range supported by the module.
+ *
+ * @param  {Number} input the decimal number to convert to a string representation
+ * @return {Error} if the input is within the supported range return null, otherwise
+ *  return an error object otherwise
  */
-
 function validateSize(input) {
   var error, absInput, maxSize;
   absInput = Math.abs(input)
@@ -55,7 +54,7 @@ function validateSize(input) {
   if (absInput >= maxSize) {
     var err = 'input parameter must be in range [-' + maxSize + ', ' + maxSize + ']'
     error = {
-      message: 'failed to get string representation of number',
+      message: 'failed to get string representation of number, range must be within negavtive to position trillions (10^15)',
       error: err,
       stack: new Error().stack
 }
